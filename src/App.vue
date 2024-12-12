c1ed635f444f22f851572941b617206a7891b022
darkgreenchalk <darkgreenchalk@gmail.com>
2024-12-11 19:50:42 -0500

    template from https://www.jw.org/en/library/books/enjoy-life-forever/

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,70 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import SynopsisDiv from './components/SynopsisDiv.vue'
+
+const lessons = [
+  "How Can the Bible Help You?",
+  "The Bible Gives Hope",
+  "Can You Trust the Bible?",
+  "Who Is God?",
+  "The Bible Is God's Message to Us",
+  "How Did Life Begin?",
+  "What Is Jehovah Like?",
+  "You Can Be Jehovah's Friend",
+  "Draw Close to God Through Prayer",
+  "How Can the Meetings of Jehovah's Witnesses Benefit You?",
+  "How You Can Get the Most Out of the Bible",
+  "What Will Help You to Keep Studying the Bible?",
+  "Section 1 Review",
+]
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="regionHeader" class="jsLockedChrome">
+    <div class="wrapper">
+      <div class="wrapperShadow">
+        <header id="siteHeader">
+          <div id="siteLogo">
+            <a href="https://vite.dev" target="_blank">
+              <img src="/vite.svg" class="logo" alt="Vite logo" />
+            </a>
+          </div>
+          <div class="siteNameContainer">
+            <h2 id="siteName">Seiwa</h2>
+          </div>
+        </header>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="regionMain">
+    <div class="wrapper">
+      <div class="wrapperShadow cms-clearfix">
+        <main role="main" id="content" class="grid-layout--pub-sidebar grid-showLSR">
+          <div class="main-wrapper">
+            <figure id="articleTopRelatedImage" class="article-top-related-image textSizeIncrement dir-ltr" dir="ltr">
+              <img src="./assets/trail.jpg" />
+            </figure>
+            <article id="article" dir="ltr" class="layout-reading layout-sidebar">
+              <div class="textSizeIncrement">
+                <h1>Enjoy Life Forever!—Introductory Bible Lessons</h1>
+              </div>
+              <p>This textbook is designed to be used during a personal Bible study as part of our free Bible study
+                program.</p>
+              <div class="toc cms-clearfix">
+                <SynopsisDiv link="#" img-src="/vue-vite/src/assets/ipad.jpg"
+                  text="How to get the most out of this Bible course" />
+                <SynopsisDiv link="#" img-src="/vue-vite/src/assets/reading.jpg" text="Section 1" />
+                <h2 class="pubviewsTOCSubsectionTitle">LESSONS</h2>
+                <hr>
+                <SynopsisDiv v-for="(text, index) in lessons" link="#"
+                  img-src="/vue-vite/src/assets/open book.jpg" :title="'LESSON ' + (index + 1)"
+                  :text="text" />
+              </div>
+            </article>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/SynopsisDiv.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+defineProps<{
+    link?: string
+    imgSrc?: string
+    title?: string
+    text?: string
+}>()
+
+</script>
+
+<template>
+    <div class="synopsis sqs dir-ltr">
+        <div class="syn-img sqs">
+            <a :href="link || '#'">
+                <img :src="imgSrc" />
+            </a>
+        </div>
+
+        <div class="syn-body sqs">
+            <p class="contextTitle">
+                {{ title }}
+            </p>
+            <h2>
+                <a :href="link">
+                    {{ text }}
+                </a>
+            </h2>
+        </div>
+    </div>
+</template>
