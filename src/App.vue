7d1b2cdc9f4f9a3daf2cda0dca65b4c7bb208217
darkgreenchalk <darkgreenchalk@gmail.com>
2025-01-10 13:45:36 -0500

    resolve: [Vue Router warn]: No match found for location with path "/"

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,42 @@
 
 <template>
   <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <br>
-    <RouterLink :to="{ name: 'demo'}">Demo</RouterLink>
-    <br>
-    <RouterLink :to="{ name: 'videos'}">Videos</RouterLink>
+    <li><RouterLink to="/">Home</RouterLink></li>
+    <li><RouterLink :to="{ name: 'demo'}">Demo</RouterLink></li>
+    <li><RouterLink :to="{ name: 'videos'}">Videos</RouterLink></li>
   </nav>
   <main>
     <RouterView />
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  margin-bottom: 20px;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.router-link-active {
+  background-color: #04AA6D;
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
+import HomeView from './views/HomeView.vue'
 import DemoPageView from './views/DemoPageView.vue'
 import VideosView from './views/VideosView.vue'
 
 const routes = [
+    { path: '/', name: 'home', component: HomeView },
     { path: '/demo', name: 'demo', component: DemoPageView },
     { path: '/videos', name: 'videos', component: VideosView },
 ]
