9e5e954c6ff3fe88606ca5f54c6b6e0418b0c81d
darkgreenchalk <darkgreenchalk@gmail.com>
2025-01-10 13:44:09 -0500

    add upload video and view through presigned

--- src/components/UploadVideo.vue
+++ src/components/UploadVideo.vue
@@ -1,17 +1,29 @@
 <template>
     <div>
-        <div :if="message">
-            {{message}}
+        <div class="w3-modal" onclick="this.style.display='none'">
+            <span class="w3-button w3-hover-red w3-xlarge w3-display-topright">&times;</span>
+            <div class="w3-modal-content w3-animate-zoom">
+                <!-- <img id="img01" style="width:100%"> -->
+                test
+            </div>
         </div>
-        <div :if="error">
+        <div v-if="message">
+            {{ message }}
+        </div>
+        <div v-if="error">
             <span color="red">Oh No!</span>
         </div>
-        <form @submit.prevent="sendFile" enctype="multipart/form-data">
-            <input type="file" name="file" accept="video/*" @change="selectFile" />
-            <hr>
-            <!-- <input type="text" name="caption" placeholder="Caption" /> -->
-            <button type="submit">Submit</button>
-        </form>
+        <button @click="activateUpload" v-if="!isUpload" class="spacing">
+            + Upload
+        </button>
+        <div v-if="isUpload" class="spacing">
+            <form @submit.prevent="sendFile" enctype="multipart/form-data">
+                <input type="file" name="file" accept="video/*" @change="selectFile" />
+                <br>
+                <!-- <input type="text" name="caption" placeholder="Caption" /> -->
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -20,35 +32,83 @@ import { ref, computed } from "vue";
 import axios from "axios";
 
 const file = ref({ name: "" });
-const fileName = computed(() => file.value?.name)
-const fileExtension = computed(() => fileName.value?.substr(fileName.value?.lastIndexOf(".") + 1))
+const fileName = computed(() => file.value.name)
+const fileExtension = computed(() => fileName.value.substr(fileName.value.lastIndexOf(".") + 1))
 
 const message = ref("")
-let error = false
+const error = ref(false)
+const isUpload = ref(false)
 
-const selectFile = async event => {
+const activateUpload = () => {
+    isUpload.value = true
+}
+
+async function selectFile(event: any) {
     file.value = event.target.files[0]
     message.value = ""
-    error = false
+    error.value = false
     // todo: multiple
 }
 
 async function sendFile() {
-    console.log(`fileName: ${fileName.value}`)
-    console.log(`fileExtension: ${fileExtension.value}`)
     const formData = new FormData();
     formData.append("file", file.value)
     formData.append("videoName", fileName.value)
     formData.append("extension", fileExtension.value)
-    // console.log(formData)
     try {
+        // todo: export to .env file
         await axios.post("http://localhost:3000/api/upload", formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+        console.log(`uploading ${fileName.value}`)
         message.value = "Hurray"
-        error = false
+        error.value = false
     } catch (err) {
         console.log(err)
         message.value = "Something went wrong"
-        error = true
+        error.value = true
     }
+
 }
 </script>
+
+<style scoped>
+.spacing {
+    margin-bottom: 10px;
+}
+
+.w3-modal {
+    z-index: 3;
+    display: none;
+    padding-top: 100px;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgb(0, 0, 0);
+    background-color: rgba(0, 0, 0, 0.4)
+}
+
+.w3-modal-content {
+    margin: auto;
+    background-color: #fff;
+    position: relative;
+    padding: 0;
+    outline: 0;
+    width: 600px
+}
+
+.w3-animate-zoom {
+    animation: animatezoom 0.6s
+}
+
+@keyframes animatezoom {
+    from {
+        transform: scale(0)
+    }
+
+    to {
+        transform: scale(1)
+    }
+}
+</style>
--- src/views/VideosView.vue
+++ src/views/VideosView.vue
@@ -1,40 +1,75 @@
 <script setup lang="ts">
 import UploadVideo from '../components/UploadVideo.vue'
 import axios from "axios";
-
 import { ref, watchEffect } from 'vue'
 
-const API_URL = `http://localhost:3000/api/videoTitles`
 const vids = ref([])
-
+const HOST_URL = 'http://localhost:3000'
+const VIDEO_TITLES_API_URL = '/api/videoTitles'
 watchEffect(async () => {
-  // this effect will run immediately and then
-  // re-run whenever vids.value changes
-  const response = await axios.get(API_URL)
-  vids.value = response.data
+    // this effect will run immediately and then
+    // re-run whenever vids.value changes
+    const response = await axios.get(`${HOST_URL}${VIDEO_TITLES_API_URL}`)
+    vids.value = response.data
 })
+
+const vidSrc = ref('')
+const PRESIGNED_URL_API = '/api/signedUrl/'
+const changeVidSrc = async (title: string) => {
+    // const title = 'SampleVideo_1280x720_1mb.mp4'
+    const response = await axios.get(`${HOST_URL}${PRESIGNED_URL_API}${title}`)
+    vidSrc.value = response.data
+    // cache urls? also need to check expiration
+    // could cleanse every hour... that would be a constant job though
+}
+
 </script>
 
 <template>
-    <UploadVideo />
-    <hr>
-    <div v-for="vid in vids">
-        {{ vid }}
+    <div class="row">
+        <div class="column-left">
+            <UploadVideo />
+            <div v-for="vid in vids" class="video-link">
+                <a @click="() => changeVidSrc(vid)">{{ vid }}</a>
+                <!-- <VideoLink :text="vid" /> -->
+            </div>
+        </div>
+        <div class="column-right">
+            <video :src="vidSrc" width="100%" class="player" controls controlsList="nodownload"
+            oncontextmenu="return false;">
+            </video>
+            <!-- <iframe class="player" width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe> -->
+            <!-- <iframe class="player3" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe> -->
+        </div>
     </div>
-    <!-- <iframe class="player" width="450" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe> -->
-    <!-- <iframe class="player3" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe> -->
-    <!-- <video :src="vid" width="420" class="player" controls controlsList="nodownload"
-        oncontextmenu="return false;"></video> -->
 </template>
 
 <style scoped>
+.column-left {
+    float: left;
+    width: 30%;
+}
+
+.column-right {
+    float: right;
+    width: 70%;
+}
+
+.column-right video {
+    display: block;
+}
+
+.video-link {
+    margin-bottom: 10px;
+}
+
 .player {
     width: 420;
     height: 315;
 }
 
 .player3 {
-    width: 420;
-    height: 315;
+    width: 800px;
+    height: 600px;
 }
 </style>
